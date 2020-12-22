8f459850d9cbd2e3aa0a16a4eed8a1e56f5bd04c
topheryun <46732742+topheryun@users.noreply.github.com>
2020-12-21 16:40:29 -0800

    testing stuff

--- src/test/java/com/revature/service/impl/ERSManipServiceImplTest.java
+++ src/test/java/com/revature/service/impl/ERSManipServiceImplTest.java
@@ -1,57 +1,57 @@
-package com.revature.service.impl;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.revature.exception.BusinessException;
-import com.revature.model.Status;
-import com.revature.service.ERSManipService;
-
-class ERSManipServiceImplTest {
-
-	private static ERSManipService ersManipService;
-	
-	@BeforeAll
-	public static void setUp() {
-		ersManipService = new ERSManipServiceImpl();
-	}
-	
-	@Test
-	void testCreateReimbursementRequestWrongUsername() throws BusinessException {
-		boolean isCreated = ersManipService.createReimbursementRequest("topher", 10f, "Other", "");
-		assertFalse(isCreated);
-	}
-	
-	@Test
-	void testCreateReimbursementRequestInvalidUsername() throws BusinessException {
-		boolean isCreated = ersManipService.createReimbursementRequest("", 10f, "Other", "");
-		assertFalse(isCreated);
-	}
-	
-	@Test
-	void testCreateReimbursementRequestInvalidAmount() throws BusinessException {
-		boolean isCreated = ersManipService.createReimbursementRequest("tophe", -5, "Other", "");
-		assertFalse(isCreated);
-	}
-	
-	@Test
-	void testCreateReimbursementRequestInvalidType() throws BusinessException {
-		boolean isCreated = ersManipService.createReimbursementRequest("tophe", 10f, "RandomString", "");
-		assertFalse(isCreated);
-	}
-	
-	@Test
-	void testResolveTicketStatusWrongId() throws BusinessException {
-		boolean isResolved = ersManipService.resolveTicketStatus(Status.PENDING, -1, 2);
-		assertFalse(isResolved);
-	}
-	
-	@Test
-	void testResolveTicketStatusWrongResolver() throws BusinessException {
-		boolean isResolved = ersManipService.resolveTicketStatus(Status.PENDING, 1, -1);
-		assertFalse(isResolved);
-	}
-
-}
+//package com.revature.service.impl;
+//
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//
+//import com.revature.exception.BusinessException;
+//import com.revature.model.Status;
+//import com.revature.service.ERSManipService;
+//
+//class ERSManipServiceImplTest {
+//
+//	private static ERSManipService ersManipService;
+//	
+//	@BeforeAll
+//	public static void setUp() {
+//		ersManipService = new ERSManipServiceImpl();
+//	}
+//	
+//	@Test
+//	void testCreateReimbursementRequestWrongUsername() throws BusinessException {
+//		boolean isCreated = ersManipService.createReimbursementRequest("topher", 10f, "Other", "");
+//		assertFalse(isCreated);
+//	}
+//	
+//	@Test
+//	void testCreateReimbursementRequestInvalidUsername() throws BusinessException {
+//		boolean isCreated = ersManipService.createReimbursementRequest("", 10f, "Other", "");
+//		assertFalse(isCreated);
+//	}
+//	
+//	@Test
+//	void testCreateReimbursementRequestInvalidAmount() throws BusinessException {
+//		boolean isCreated = ersManipService.createReimbursementRequest("tophe", -5, "Other", "");
+//		assertFalse(isCreated);
+//	}
+//	
+//	@Test
+//	void testCreateReimbursementRequestInvalidType() throws BusinessException {
+//		boolean isCreated = ersManipService.createReimbursementRequest("tophe", 10f, "RandomString", "");
+//		assertFalse(isCreated);
+//	}
+//	
+//	@Test
+//	void testResolveTicketStatusWrongId() throws BusinessException {
+//		boolean isResolved = ersManipService.resolveTicketStatus(Status.PENDING, -1, 2);
+//		assertFalse(isResolved);
+//	}
+//	
+//	@Test
+//	void testResolveTicketStatusWrongResolver() throws BusinessException {
+//		boolean isResolved = ersManipService.resolveTicketStatus(Status.PENDING, 1, -1);
+//		assertFalse(isResolved);
+//	}
+//
+//}
--- src/test/java/com/revature/service/impl/ERSSearchServiceImplTest.java
+++ src/test/java/com/revature/service/impl/ERSSearchServiceImplTest.java
@@ -1,115 +1,115 @@
-package com.revature.service.impl;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.revature.exception.BusinessException;
-import com.revature.model.Reimbursement;
-import com.revature.model.Role;
-import com.revature.model.Status;
-import com.revature.service.ERSSearchService;
-
-class ERSSearchServiceImplTest {
-
-	private static ERSSearchService ersSearchService;
-	
-	@BeforeAll
-	public static void setUp() {
-		ersSearchService = new ERSSearchServiceImpl();
-	}
-	
-	@Test
-	void testGetAllEmployeesTicketsInvalidId() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(-1);
-		assertNull(reimbList);
-	}
-	
-	@Test
-	void testGetAllEmployeesTickets() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(1);
-		assertNotEquals(0, reimbList.size());
-	}
-	
-	@Test
-	void testGetAllEmployeesTicketsNonExistantUser() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(999);
-		assertNull(reimbList);
-	}
-	
-	@Test
-	void testGetAllTickets() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllTickets();
-		assertNotEquals(0,reimbList.size());
-	}
-	
-	@Test
-	void testGetIdByUsernameNullUsername() throws BusinessException {
-		int userId = ersSearchService.getIdByUsername(null);
-		assertEquals(0, userId);
-	}
-	
-	@Test
-	void testGetIdByUsername() throws BusinessException {
-		int userId = ersSearchService.getIdByUsername("tophe");
-		assertEquals(1, userId);
-	}
-	
-	@Test
-	void testVerifyLogin() throws BusinessException {
-		boolean checkLogin = ersSearchService.verifyLogin("tophe", "pass");
-		assertTrue(checkLogin);
-	}
-	
-	@Test
-	void testVerifyLoginInvalidUsername() throws BusinessException {
-		boolean checkLogin = ersSearchService.verifyLogin(null, "pass");
-		assertFalse(checkLogin);
-	}
-	
-	@Test
-	void testVerifyLoginInvalidPassword() throws BusinessException {
-		boolean checkLogin = ersSearchService.verifyLogin("tophe", null);
-		assertFalse(checkLogin);
-	}
-	
-	@Test
-	void testVerifyLoginWrongUsername() throws BusinessException {
-		boolean checkLogin = ersSearchService.verifyLogin("topher", "pass");
-		assertFalse(checkLogin);
-	}
-	
-	@Test
-	void testGetAllTicketsByStatusPending() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllTicketsByStatus(Status.PENDING);
-		assertNotEquals(0, reimbList.size());
-	}
-	
-	@Test
-	void testGetAllTicketsByStatusNull() throws BusinessException {
-		List<Reimbursement> reimbList = ersSearchService.getAllTicketsByStatus(null);
-		assertNull(reimbList);
-	}
-	
-	@Test
-	void testGetRoleByUsernameEmployee() throws BusinessException {
-		Role role = ersSearchService.getRoleByUsername("tophe");
-		boolean checkRole = false;
-		if (role == Role.EMPLOYEE) checkRole = true;
-		assertTrue(checkRole);
-	}
-	
-	@Test
-	void testGetRoleByUsernameWrongUsername() throws BusinessException {
-		Role role = ersSearchService.getRoleByUsername("topher");
-		assertNull(role);
-	}
-
-}
+//package com.revature.service.impl;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertNull;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//
+//import com.revature.exception.BusinessException;
+//import com.revature.model.Reimbursement;
+//import com.revature.model.Role;
+//import com.revature.model.Status;
+//import com.revature.service.ERSSearchService;
+//
+//class ERSSearchServiceImplTest {
+//
+//	private static ERSSearchService ersSearchService;
+//	
+//	@BeforeAll
+//	public static void setUp() {
+//		ersSearchService = new ERSSearchServiceImpl();
+//	}
+//	
+//	@Test
+//	void testGetAllEmployeesTicketsInvalidId() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(-1);
+//		assertNull(reimbList);
+//	}
+//	
+//	@Test
+//	void testGetAllEmployeesTickets() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(1);
+//		assertNotEquals(0, reimbList.size());
+//	}
+//	
+//	@Test
+//	void testGetAllEmployeesTicketsNonExistantUser() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllEmployeesTickets(999);
+//		assertNull(reimbList);
+//	}
+//	
+//	@Test
+//	void testGetAllTickets() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllTickets();
+//		assertNotEquals(0,reimbList.size());
+//	}
+//	
+//	@Test
+//	void testGetIdByUsernameNullUsername() throws BusinessException {
+//		int userId = ersSearchService.getIdByUsername(null);
+//		assertEquals(0, userId);
+//	}
+//	
+//	@Test
+//	void testGetIdByUsername() throws BusinessException {
+//		int userId = ersSearchService.getIdByUsername("tophe");
+//		assertEquals(1, userId);
+//	}
+//	
+//	@Test
+//	void testVerifyLogin() throws BusinessException {
+//		boolean checkLogin = ersSearchService.verifyLogin("tophe", "pass");
+//		assertTrue(checkLogin);
+//	}
+//	
+//	@Test
+//	void testVerifyLoginInvalidUsername() throws BusinessException {
+//		boolean checkLogin = ersSearchService.verifyLogin(null, "pass");
+//		assertFalse(checkLogin);
+//	}
+//	
+//	@Test
+//	void testVerifyLoginInvalidPassword() throws BusinessException {
+//		boolean checkLogin = ersSearchService.verifyLogin("tophe", null);
+//		assertFalse(checkLogin);
+//	}
+//	
+//	@Test
+//	void testVerifyLoginWrongUsername() throws BusinessException {
+//		boolean checkLogin = ersSearchService.verifyLogin("topher", "pass");
+//		assertFalse(checkLogin);
+//	}
+//	
+//	@Test
+//	void testGetAllTicketsByStatusPending() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllTicketsByStatus(Status.PENDING);
+//		assertNotEquals(0, reimbList.size());
+//	}
+//	
+//	@Test
+//	void testGetAllTicketsByStatusNull() throws BusinessException {
+//		List<Reimbursement> reimbList = ersSearchService.getAllTicketsByStatus(null);
+//		assertNull(reimbList);
+//	}
+//	
+//	@Test
+//	void testGetRoleByUsernameEmployee() throws BusinessException {
+//		Role role = ersSearchService.getRoleByUsername("tophe");
+//		boolean checkRole = false;
+//		if (role == Role.EMPLOYEE) checkRole = true;
+//		assertTrue(checkRole);
+//	}
+//	
+//	@Test
+//	void testGetRoleByUsernameWrongUsername() throws BusinessException {
+//		Role role = ersSearchService.getRoleByUsername("topher");
+//		assertNull(role);
+//	}
+//
+//}
